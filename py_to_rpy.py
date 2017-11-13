7a7ce2fa6b4c02a67ae7545eec663c739bdfe816
Joshua Fehler <jsfehler@gmail.com>
2017-11-13 00:14:20 -0500

    --dest option can be used to place transpiled files in a folder

--- py_to_rpy.py
+++ py_to_rpy.py
@@ -1,19 +1,39 @@
 import argparse
+import errno
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("files", help="List of files to convert", nargs="+")
+parser.add_argument(
+    "--dest",
+    help="Folder to place transpiled files into",
+    nargs="?"
+)
 
 extra_indent = "    "
 
 
-def py_to_rpy(filename):
+def py_to_rpy(filename, dest=None):
     """Transpiles a Python file to a Ren'Py file.
-    
+
     Creates a new .rpy file with an 'init python' block.
 
     """
+    if dest:
+        dest_string = "{}/".format(dest)
+        # Create the dist directory if it doesn't already exist
+        try:
+            os.makedirs(dest)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    else:
+        dest_string = ""
+
+    rpy_file_location = "{}{}.rpy".format(dest_string, filename)
+    
     with open(filename + ".py", "r") as py_file:
-        with open(filename + ".rpy", "w") as rpy_file:
+        with open(rpy_file_location, "w") as rpy_file:
             rpy_file.write("init python:")
             rpy_file.write("\n")
             for line in py_file:
@@ -25,5 +45,6 @@ def py_to_rpy(filename):
 
 if __name__ == "__main__":
     file_list = parser.parse_args().files
+    dest_dir = parser.parse_args().dest
     for item in file_list:
-        py_to_rpy(item)
+        py_to_rpy(item, dest=dest_dir)
--- tests/test_transpile.py
+++ tests/test_transpile.py
@@ -1,21 +1,54 @@
 import os
+import shutil
 
 from ..py_to_rpy import py_to_rpy
 
 
-def test_py_to_rpy(request):
+def test_single_file(request):
     """Ensure the result matches the pre-cooked file."""
     # Cleanup
     def fin():
         os.remove('dummy_file.rpy')
 
-    request.addfinalizer(fin)    
+    request.addfinalizer(fin)
 
     # Start test
     py_to_rpy('dummy_file')
-    
+
     with open('expected_dummy_file.rpy') as expected_rpy_file:
         with open('dummy_file.rpy') as rpy_file:
             f1 = expected_rpy_file.read()
             f2 = rpy_file.read()
             assert f1 == f2
+
+
+def test_dest_option_new_folder(request):
+    """Ensure the dest option works when the taget directory does not exist.
+       New files should be placed in the dest folder.
+    """
+    # Cleanup
+    def fin():
+        shutil.rmtree('dummy_folder')
+
+    request.addfinalizer(fin)
+
+    # Start test
+    py_to_rpy('dummy_file', dest='dummy_folder')
+
+    assert ['dummy_file.rpy'] == os.listdir('dummy_folder')
+
+
+def test_dest_option_existing_folder(request):
+    """Ensure the dest option works when the taget directory already exists.
+       New files should be placed in the dest folder.
+    """
+    # Cleanup
+    def fin():
+        os.remove('existing_dummy_folder/dummy_file.rpy')
+
+    request.addfinalizer(fin)
+
+    # Start test
+    py_to_rpy('dummy_file', dest='existing_dummy_folder')
+
+    assert ['dummy_file.rpy'] == os.listdir('existing_dummy_folder')
